db1cf345f2f9cacc5fbd788f9ccc7568b74571b7
kwkang <kwkang@nm-gurobal.com>
2020-04-16 15:08:02 +0900

    Update install-multitail.sh

--- install-multitail.sh
+++ install-multitail.sh
@@ -8,3 +8,10 @@ wget https://www.vanheusden.com/multitail/multitail-6.5.0.tgz
 tar xvf multitail-6.5.0.tgz
 cd multitail-6.5.0
 make install
+
+cp /usr/local/etc/multitail.conf.new /usr/local/etc/multitail.conf
+echo "colorscheme:mylog" >> /usr/local/etc/multitail.conf
+echo "cs_re:magenta,,bold:^.*\[critical\].*" >> /usr/local/etc/multitail.conf
+echo "cs_re:red,,bold:^.*\[error\].*" >> /usr/local/etc/multitail.conf
+echo "cs_re:yellow:^.*\[warning\].*" >> /usr/local/etc/multitail.conf
+echo "cs_re:green:^.*\[info\].*" >> /usr/local/etc/multitail.conf
