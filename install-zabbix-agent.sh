a6c4ddd784271f24084a3af33ddae1c80974ba57
kwkang <kwkang@nm-gurobal.com>
2020-04-17 17:34:56 +0900

    Update install-zabbix-agent.sh

--- install-zabbix-agent.sh
+++ install-zabbix-agent.sh
@@ -2,3 +2,5 @@ rpm -Uvh https://repo.zabbix.com/zabbix/4.2/rhel/7/x86_64/zabbix-release-4.2-2.e
 yum clean all
 yum install zabbix-agent -y
 yum update -y
+firewall-cmd --zone=public --add-port=10050/tcp --permanent
+firewall-cmd --reload
