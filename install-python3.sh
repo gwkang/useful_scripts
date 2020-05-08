95394e9aaa66cc8bc0a4e422fa78da9ac1a8151e
kwkang <kwkang@nm-gurobal.com>
2020-05-08 15:31:22 +0900

    Update install-python3.sh

--- install-python3.sh
+++ install-python3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yum -y install zlib-devel libffi-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel
+yum -y install zlib-devel libffi-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel wget
 wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tgz
 tar xvf Python-3.7.3.tgz
 cd Python-3.7.3; ./configure --enable-optimizations && make altinstall
