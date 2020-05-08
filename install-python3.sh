5fce02226afd7e168cf56940a9ffa2a85fb06042
kwkang <kwkang@nm-gurobal.com>
2020-05-08 15:33:03 +0900

    Update install-python3.sh

--- install-python3.sh
+++ install-python3.sh
@@ -5,5 +5,6 @@ wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tgz
 tar xvf Python-3.7.3.tgz
 cd Python-3.7.3; ./configure --enable-optimizations && make altinstall
 ln -s /usr/local/bin/python3.7 /usr/local/bin/python3
+cd..; rm -rf Python-3.7.3; rm -f Python-3.7.3.tgz
 wget https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py
