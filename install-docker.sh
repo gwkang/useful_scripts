79b67ad14f39c0ec3aa5e03aaa61b5a678d55d28
Kiwan Kang <kwkang@nm-gurobal.com>
2020-09-08 15:01:47 +0900

    Create install-docker.sh

---
+++ install-docker.sh
@@ -0,0 +1,6 @@
+yum remove -y docker*
+yum install -y yum-utils
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y docker-ce docker-ce-cli containerd.io
+systemctl enable docker
+systemctl start docker
